038f98bccd3e5159caf272a92e8dfb263b50bcd7
Pooya Parsa <pooya@pi0.io>
2023-10-19 13:41:22 +0200

    remove types for now

--- test/fixture/config.ts
+++ test/fixture/config.ts
@@ -1,14 +1,9 @@
-import { createDefineConfig } from "../../src";
-import type { SourceOptions } from "../../src";
-
-const defineConfig = createDefineConfig();
-
-export default defineConfig({
+export default {
   theme: "./theme",
   extends: [
     ["c12-npm-test"],
     ["gh:unjs/c12/test/fixture/_github#main", { giget: {} }],
-  ] as [string, SourceOptions][] /* TODO: Auto type */,
+  ],
   $test: {
     extends: ["./config.dev"],
     envConfig: true,
@@ -21,4 +16,4 @@ export default defineConfig({
   // foo: "bar",
   // x: "123",
   array: ["a"],
-});
+};
