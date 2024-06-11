8d4bf3e3f23550b138edfb0a40fc0ce8fb2b8bcb
Pooya Parsa <pooya@pi0.io>
2024-06-12 01:04:06 +0200

    feat: config update util (#162)

---
+++ test/update.test.ts
@@ -0,0 +1,42 @@
+import { fileURLToPath } from "node:url";
+import { expect, it, describe, beforeAll } from "vitest";
+import { normalize } from "pathe";
+import { updateConfig } from "../src/update";
+import { readFile, rm } from "node:fs/promises";
+import { existsSync } from "node:fs";
+
+const r = (path: string) =>
+  normalize(fileURLToPath(new URL(path, import.meta.url)));
+
+describe("update config file", () => {
+  const tmpDir = r("./.tmp");
+  beforeAll(async () => {
+    await rm(tmpDir, { recursive: true }).catch(() => {});
+  });
+  it("create new config", async () => {
+    let onCreateFile;
+    const res = await updateConfig({
+      cwd: tmpDir,
+      configFile: "foo.config",
+      onCreate: ({ configFile }) => {
+        onCreateFile = configFile;
+        return "export default { test: true }";
+      },
+      onUpdate: (config) => {
+        config.test2 = false;
+      },
+    });
+    expect(res.created).toBe(true);
+    expect(res.configFile).toBe(r("./.tmp/foo.config.ts"));
+    expect(onCreateFile).toBe(r("./.tmp/foo.config.ts"));
+
+    expect(existsSync(r("./.tmp/foo.config.ts"))).toBe(true);
+    const contents = await readFile(r("./.tmp/foo.config.ts"), "utf8");
+    expect(contents).toMatchInlineSnapshot(`
+      "export default {
+        test: true,
+        test2: false
+      };"
+    `);
+  });
+});
