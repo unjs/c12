13fb25160b9095fff4b1c1431c19599646bde130
Inesh Bose <dev@inesh.xyz>
2024-12-16 18:56:09 +0000

    docs: move options description to JSDoc

--- src/types.ts
+++ src/types.ts
@@ -98,25 +98,77 @@ export interface LoadConfigOptions<
   T extends UserInputConfig = UserInputConfig,
   MT extends ConfigLayerMeta = ConfigLayerMeta,
 > {
+  /**
+   * Configuration base name.
+   *
+   * @default 'config'
+   */
   name?: string;
+  /**
+   * Resolve configuration from this working directory.
+   *
+   * @default process.cwd()
+   */
   cwd?: string;
 
+  /**
+   * Configuration file name without extension. Default is generated from `name` (f.e., if `name` is `foo`, the config file will be => `foo.config`).
+   *
+   * Set to `false` to avoid loading the config file.
+   */
   configFile?: string;
 
+  /**
+   * RC Config file name. Default is generated from `name` (name=foo => `.foorc`).
+   *
+   * Set to `false` to disable loading RC config.
+   */
   rcFile?: false | string;
+  /**
+   * Load RC config from the workspace directory and the user's home directory. Only enabled when `rcFile` is provided. Set to `false` to disable this functionality.
+   */
   globalRc?: boolean;
 
+  /**
+   * Loads `.env` file if enabled. It is disabled by default.
+   */
   dotenv?: boolean | DotenvOptions;
 
+  /**
+   * Environment name used for [environment specific configuration](#environment-specific-configuration).
+   *
+   * The default is `process.env.NODE_ENV`. You can set `envName` to `false` or an empty string to disable the feature.
+   */
   envName?: string | false;
 
+  /**
+   * Loads config from nearest `package.json` file. It is disabled by default.
+   *
+   * If `true` value is passed, c12 uses `name` field from `package.json`.
+   *
+   * You can also pass either a string or an array of strings as a value to use those fields.
+   */
   packageJson?: boolean | string | string[];
 
+  /**
+   * Specify default configuration. It has the **lowest** priority and is applied **after extending** config.
+   */
   defaults?: T;
 
+  /**
+   * Specify default configuration. It is applied **before** extending config.
+   */
   defaultConfig?: ResolvableConfig<T>;
+  /**
+   * Specify override configuration. It has the **highest** priority and is applied **before extending** config.
+   */
   overrides?: ResolvableConfig<T>;
 
+  /**
+   * Exclude environment-specific and built-in keys start with `$` in the resolved config.
+   *
+   * @default false
+   */
   omit$Keys?: boolean;
 
   resolve?: (
@@ -128,13 +180,32 @@ export interface LoadConfigOptions<
     | ResolvedConfig<T, MT>
     | Promise<ResolvedConfig<T, MT> | undefined | null>;
 
+  /**
+   * Custom [`unjs/jiti`](https://github.com/unjs/jiti) instance used to import configuration files.
+   */
   jiti?: Jiti;
+  /**
+   * Custom [`unjs/jiti`](https://github.com/unjs/jiti) options to import configuration files.
+   */
   jitiOptions?: JitiOptions;
 
+  /**
+   * Options passed to [`unjs/giget`](https://github.com/unjs/giget) when extending layer from git source.
+   */
   giget?: false | DownloadTemplateOptions;
 
+  /**
+   * Custom options merger function. Default is [`defu`](https://github.com/unjs/defu).
+   *
+   * **Note:** Custom merge function should deeply merge options with arguments high -> low priority.
+   */
   merger?: (...sources: Array<T | null | undefined>) => T;
 
+  /**
+   * Allow extending of configuration.
+   *
+   * @see https://github.com/unjs/c12#extending-configuration
+   */
   extend?:
     | false
     | {
