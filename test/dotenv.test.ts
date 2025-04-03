80a7afbb22b93f5cdd40e570a0e2f674976710f5
Daniel Roe <daniel@roe.dev>
2025-04-03 13:16:30 +0100

    test: use different tmp dir

--- test/dotenv.test.ts
+++ test/dotenv.test.ts
@@ -1,38 +1,39 @@
 import { fileURLToPath } from "node:url";
-import { expect, it, describe, beforeAll } from "vitest";
+import { expect, it, describe } from "vitest";
 import { normalize } from "pathe";
 import { mkdir, rm, writeFile } from "node:fs/promises";
 import { setupDotenv } from "../src";
+import { beforeEach } from "node:test"
 
 const r = (path: string) =>
   normalize(fileURLToPath(new URL(path, import.meta.url)));
 
 describe("update config file", () => {
-  const tmpDir = r("./.tmp");
-  beforeAll(async () => {
-    await rm(tmpDir, { recursive: true }).catch(() => {});
-    await mkdir(tmpDir, { recursive: true }).catch(() => {});
+  const tmpDir = r("./.tmp-config");
+  beforeEach(async () => {
+    await rm(tmpDir, { recursive: true, force: true });
+    await mkdir(tmpDir, { recursive: true });
   });
   it("should read .env file into process.env", async () => {
     await setupDotenv({ cwd: tmpDir });
     expect(process.env.dotenv).toBeUndefined();
 
-    await writeFile(r("./.tmp/.env"), "dotenv=123");
+    await writeFile(r("./.tmp-config/.env"), "dotenv=123");
     await setupDotenv({ cwd: tmpDir });
     expect(process.env.dotenv).toBe("123");
 
-    await writeFile(r("./.tmp/.env"), "dotenv=456");
+    await writeFile(r("./.tmp-config/.env"), "dotenv=456");
     await setupDotenv({ cwd: tmpDir });
     expect(process.env.dotenv).toBe("456");
   });
   it("should not override OS environment values", async () => {
     process.env.override = "os";
 
-    await writeFile(r("./.tmp/.env"), "override=123");
+    await writeFile(r("./.tmp-config/.env"), "override=123");
     await setupDotenv({ cwd: tmpDir });
     expect(process.env.override).toBe("os");
 
-    await writeFile(r("./.tmp/.env"), "override=456");
+    await writeFile(r("./.tmp-config/.env"), "override=456");
     await setupDotenv({ cwd: tmpDir });
     expect(process.env.override).toBe("os");
   });
